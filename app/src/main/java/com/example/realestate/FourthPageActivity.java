778cd598d9fd007795ea39372cea0f80087921b9
Oyedee <oyempemia@gmail.com>
2021-07-19 21:48:29 +0100

    RecyclerView implementation of a CardView UI component

--- app/src/main/java/com/example/realestate/FourthPageActivity.java
+++ app/src/main/java/com/example/realestate/FourthPageActivity.java
@@ -1,6 +1,8 @@
 package com.example.realestate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class FourthPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth_page);
+
+
+        final RecyclerView recyclerNotes = findViewById(R.id.list_cards);
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerNotes.setLayoutManager(linearLayoutManager);
     }
 }
