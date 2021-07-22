8aef8498aa1a28adc11f17cf161eddcbdcb965f1
Oyedee <oyempemia@gmail.com>
2021-07-22 18:27:07 +0100

    CardView UI component Update for images view

--- app/src/main/java/com/example/realestate/FourthPageActivity.java
+++ app/src/main/java/com/example/realestate/FourthPageActivity.java
@@ -1,6 +1,7 @@
 package com.example.realestate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -20,9 +21,15 @@ public class FourthPageActivity extends AppCompatActivity {
 
 
         RecyclerView recyclerNotes = findViewById(R.id.list_cards);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
-        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(FourthPageActivity.this, 1);
+        gridLayoutManager.setOrientation(GridLayoutManager.HORIZONTAL);
 
-        recyclerNotes.setLayoutManager(linearLayoutManager);
+        recyclerNotes.setLayoutManager(gridLayoutManager);
+
+        RecyclerView recyclerView = findViewById(R.id.list_images);
+        // set a GridLayoutManager with 3 number of columns
+        GridLayoutManager gridLayoutManager2 = new GridLayoutManager(FourthPageActivity.this,1);
+        gridLayoutManager2.setOrientation(LinearLayoutManager.HORIZONTAL); // set Horizontal Orientation
+        recyclerView.setLayoutManager(gridLayoutManager2); // set LayoutManager to RecyclerView
     }
 }
